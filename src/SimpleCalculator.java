ca10c2a68bde9ec17846aa5108e38c5e9071deab
Kira <datdhvinh@gmail.com>
2020-05-11 08:34:38 +0700

    may tinh don gian TDD

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,13 @@
+public class SimpleCalculator {
+    public static int add(int first, int second){
+//        throw new UnsupportedOperationException("Error");
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+//        throw new UnsupportedOperationException("Error");
+        return first - second;
+    }
+}
+
+
